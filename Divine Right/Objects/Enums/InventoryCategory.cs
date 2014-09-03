3e61c154c19298f7884e926ef17b669cd05d7795
Adrian <aadrianabela@gmail.com>
2014-09-03 20:01:52 +0200

    Added some InventoryItems

---
+++ Divine Right/Objects/Enums/InventoryCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRObjects.Enums
+{
+    public enum InventoryCategory
+    {
+        WEAPON,
+        ARMOUR,
+        LOOT,
+       // CONSUMABLES,
+       // DOCUMENTS,
+       // RESOURCES
+    }
+}
