19925cb2eb7893bd7ee1ddcc65e91bc3cc640c51
Adrian <aadrianabela@gmail.com>
2014-10-11 21:58:46 +0200

    Revert "Fixed issue with Serialization and MonoGame"
    This reverts commit 62c4a488152b34108252b937ea9f19d3c409ea2a.

--- Divine Right/DivineRightGame/GameState.cs
+++ Divine Right/DivineRightGame/GameState.cs
@@ -11,7 +11,6 @@ using System.Runtime.Serialization;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using DRObjects.DataStructures;
-using Microsoft.Xna.Framework;
 
 namespace DivineRightGame
 {
@@ -75,14 +74,6 @@ namespace DivineRightGame
             saveFile.UniverseTime = GameState.UniverseTime;
 
             IFormatter formatter = new BinaryFormatter();
-
-            SurrogateSelector ss = new SurrogateSelector();
-
-            ss.AddSurrogate(typeof(Rectangle), new StreamingContext(StreamingContextStates.All), new RectSerializationSurrogate());
-            ss.AddSurrogate(typeof(Color), new StreamingContext(StreamingContextStates.All), new ColorSerializationSurrogate());
-
-            formatter.SurrogateSelector = ss;
-
             Stream stream = new FileStream(GameState.SAVEPATH + "GameState.dvg", FileMode.Create, FileAccess.Write, FileShare.None);
             formatter.Serialize(stream, saveFile);
             stream.Close();
@@ -94,14 +85,6 @@ namespace DivineRightGame
         public static void LoadGame()
         {
             IFormatter formatter = new BinaryFormatter();
-
-            SurrogateSelector ss = new SurrogateSelector();
-
-            ss.AddSurrogate(typeof(Rectangle), new StreamingContext(StreamingContextStates.All), new RectSerializationSurrogate());
-            ss.AddSurrogate(typeof(Color), new StreamingContext(StreamingContextStates.All), new ColorSerializationSurrogate());
-
-            formatter.SurrogateSelector = ss;
-
             Stream stream = new FileStream(GameState.SAVEPATH + "GameState.dvg", FileMode.Open, FileAccess.Read, FileShare.Read);
             GameStateSaveFile obj = (GameStateSaveFile)formatter.Deserialize(stream);
             stream.Close();
--- Divine Right/DivineRightGame/LocalMap.cs
+++ Divine Right/DivineRightGame/LocalMap.cs
@@ -19,7 +19,6 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using DRObjects.DataStructures.Enum;
-using DRObjects.DataStructures;
 
 namespace DivineRightGame
 {
@@ -270,14 +269,6 @@ namespace DivineRightGame
             }
 
             IFormatter formatter = new BinaryFormatter();
-
-            SurrogateSelector ss = new SurrogateSelector();
-
-            ss.AddSurrogate(typeof(Rectangle), new StreamingContext(StreamingContextStates.All), new RectSerializationSurrogate());
-            ss.AddSurrogate(typeof(Color), new StreamingContext(StreamingContextStates.All), new ColorSerializationSurrogate());
-
-            formatter.SurrogateSelector = ss;
-
             Stream stream = new FileStream(GameState.SAVEPATH + uniqueGuid + ".dvd", FileMode.Create, FileAccess.Write, FileShare.None);
             formatter.Serialize(stream, this);
             stream.Close();
@@ -291,14 +282,6 @@ namespace DivineRightGame
         public static LocalMap DeserialiseLocalMap(Guid uniqueGuid)
         {
             IFormatter formatter = new BinaryFormatter();
-
-            SurrogateSelector ss = new SurrogateSelector();
-
-            ss.AddSurrogate(typeof(Rectangle), new StreamingContext(StreamingContextStates.All), new RectSerializationSurrogate());
-            ss.AddSurrogate(typeof(Color), new StreamingContext(StreamingContextStates.All), new ColorSerializationSurrogate());
-
-            formatter.SurrogateSelector = ss;
-
             Stream stream = new FileStream(GameState.SAVEPATH + uniqueGuid + ".dvd", FileMode.Open, FileAccess.Read, FileShare.Read);
             LocalMap obj = (LocalMap)formatter.Deserialize(stream);
             stream.Close();
