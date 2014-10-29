ad6077d105450ee2b9fe96ff37f3b0acf9e2fa6a
Adrian <aadrianabela@gmail.com>
2014-10-29 22:44:01 +0100

    You may not attack domesticated animals

--- Divine Right/Objects/Items/Archetypes/Local/LocalCharacter.cs
+++ Divine Right/Objects/Items/Archetypes/Local/LocalCharacter.cs
@@ -118,7 +118,7 @@ namespace DRObjects.Items.Archetypes.Local
             actions.AddRange(base.GetPossibleActions(actor));
 
             //Are we next to the target? Is the actor aggressive, or an animal?
-            if (this.Coordinate - actor.MapCharacter.Coordinate < 2 && (this.Actor.IsAggressive || this.Actor.IsAnimal))
+            if (this.Coordinate - actor.MapCharacter.Coordinate < 2 && (this.Actor.IsAggressive || (this.Actor.IsAnimal && !this.Actor.IsDomesticatedAnimal)))
             {
                 //Add the attack one too
                 actions.Add(ActionType.PREPARE_ATTACK);
