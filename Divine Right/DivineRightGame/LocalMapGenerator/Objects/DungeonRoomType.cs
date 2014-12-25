129d70fbdd28bb3c2aa3eb24b1272392bdf462d3
Adrian Abela <aadrianabela@gmail.com>
2014-12-25 21:32:03 +0100

    Some dungeon room types

---
+++ Divine Right/DivineRightGame/LocalMapGenerator/Objects/DungeonRoomType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivineRightGame.LocalMapGenerator.Objects
+{
+    /// <summary>
+    /// The type of Dungeon Room
+    /// </summary>
+    public enum DungeonRoomType
+    {
+        TREASURE_ROOM,
+        ARMOURY,
+        ALTAR_ROOM,
+        DARK_TEMPLE,
+        POOL_ROOM,
+        DESTROYED_CAMP_ROOM,
+        ENTRANCE_ROOM,
+        EXIT_ROOM,
+        SPAWNING_ROOM,
+
+    }
+}
