bcfef5cf099c8cb350ff03dccbf7dc46096485ae
Adrian <aadrianabela@gmail.com>
2015-01-06 19:58:46 +0100

    Preperation code for Effects

---
+++ Divine Right/Objects/ActorHandling/Effect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DRObjects.ActorHandling.Enums;
+
+namespace DRObjects.ActorHandling
+{
+    /// <summary>
+    /// Represents a temporary effect on a character
+    /// </summary>
+    public class Effect
+    {
+        public EffectName Name { get; set; }
+        public int EffectAmount { get; set; }
+        public int MinutesLeft { get; set; }
+    }
+}
---
+++ Divine Right/Objects/ActorHandling/Enums/EffectName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRObjects.ActorHandling.Enums
+{
+    public enum EffectName
+    {
+        BRAWN,
+        AGIL,
+        CHAR,
+        PERC,
+        INTEL
+    }
+}
